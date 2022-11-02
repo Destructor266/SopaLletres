1ec16957b11c7db930be4c4dce40f322506b351c
Destructor266 <jarmastertruck@gmail.com>
2022-11-02 14:19:47 +0100

    Webview,palabras en xml y boton cambia de color al cliclar

--- app/src/main/java/edu/fje/sopalletres/MainActivity.java
+++ app/src/main/java/edu/fje/sopalletres/MainActivity.java
@@ -4,7 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.webkit.GeolocationPermissions;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
@@ -12,10 +18,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     /*
     Diagrama UML
     Barra acció
+    menu
     Aleatorietat aparació mots
     Extreure mots en XML
     Ajuda amb WebView
-    SQLite
+    SQLite Puntuació
     Extreure els mots de contactes
     MVC
      */
@@ -40,4 +47,51 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         Intent intent = new Intent(this, SopaLletres.class);
         startActivity(intent);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.ajuda:
+                obrirAjuda();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void obrirAjuda() {
+        setContentView(R.layout.webview);
+        WebView navegador = (WebView) findViewById(R.id.webkit);
+
+        navegador.setWebViewClient(new WebViewClient());
+        navegador.loadData("<html>" +
+                "                   <body>" +
+                "                       <h1>Benvinguts a la ajuda de la Sopa de lletres</h1>" +
+                "                       <p>En aquesta secció s'explicara com funciona aquesta sopa de lletres<p>" +
+                "                       <p>El primer de tot per poder començar a jugar has d'anar a la pantalla de benvinguda de l'aplicació y premer sobre el boto de jugar<p>" +
+                "                       <p>Una vegada ho hagis fet accediras al joc</p>" +
+                "                       <h2>Funcionament del joc:</h2>" +
+                "                       <ul>" +
+                "                           <li>Timer:</li>" +
+                "                               <p>La sopa de lletres te un temps limit en la part superior de la pantalla, quan aquest temps acaba finalitza el joc</p>" +
+                "                           <li>Tauler:</li>" +
+                "                               <p>El joc principal, en aqui has de trobar les lletres que formen les paraules que estan repartides entre els diferents butons," +
+                "                               Es poden seleccionar fins a quatre butons a la vegada, sabrem que estan seleccionats perque aquest hauran cambiat de color," +
+                "                               si aquest quatre butons formen una de les paraules, cambiaran de color a una altre color y sumaran 10 punts al usuari, si no formen cap paraula retornar al estil original</p>" +
+                "                           <li>Taula de paraules a trobar:</li>" +
+                "                               <p>En aquest apartat estan les paraules que s'han de trobar a la sopa de lletres</p>" +
+                "                       </ul>" +
+                "                       <a href=\"https://es.wikipedia.org/wiki/Sopa_de_letras\">Mes informacio sobre la sopes de lletres</a" +
+                "                   </body>" +
+                "                </html>", "text/html", "UTF-8");
+
+    }
+
+
 }
--- app/src/main/java/edu/fje/sopalletres/SopaLletres.java
+++ app/src/main/java/edu/fje/sopalletres/SopaLletres.java
@@ -1,6 +1,6 @@
 package edu.fje.sopalletres;
 
-import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -10,56 +10,73 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import java.util.logging.Level;
-
 public class SopaLletres extends AppCompatActivity {
 
+    /*
     private static final String[][] Lletres = {
-            {"H", "O", "L", "A"},
-            {"A", "D", "E", "U"},
+            {"E", "Y", "", ""},
+            {"P", "O", "C", ""},
             {"C", "A", "S", "A"},
-            {"P", "O", "C", "A"},
-            {"P", "E", "R", "E"},
-            {},
+            {"", "", "", ""},
     };
-    
+     */
+
+    private int click = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.sopalletres);
 
-        GenerarSopaLleteres();
-        GenerarParaulesATrobar();
+        final String[] Lletres = getResources().getStringArray(R.array.paraules);
+
+        GenerarSopaLleteres(Lletres);
+        GenerarParaulesATrobar(Lletres);
     }
 
-    private void GenerarSopaLleteres(){
+    private void GenerarSopaLleteres(String[] Lletres){
         TableLayout TlSopaLletres = findViewById(R.id.tlSopa);
         TableRow trSopaLletres = null;
+        Button btnr = null;
+
 
         for (int columna = 0; columna < Lletres.length; columna++){
 
             trSopaLletres = new TableRow(this);
             TlSopaLletres.addView(trSopaLletres);
 
-            for (int fila = 0; fila < Lletres[columna].length; fila++){
+            for (int fila = 0; fila < Lletres[columna].length(); fila++) {
 
-                int paraula = Lletres[columna].length;
+                int paraula = Lletres[columna].length();
 
                 int LletraAleat = (int) Math.abs((Math.random() * Lletres.length - paraula));
-                int IDaleat = (int) Math.abs((Math.random() * 4));
 
-                Button btn = new Button(this);
-                btn.setText(Lletres[columna][fila]);
+                btnr = new Button(this);
+                btnr.setText(String.valueOf(Lletres[columna].charAt(fila)));
 
-                if (LletraAleat == IDaleat){
-                    //btn.setText(Lletres[LletraAleat][LletraAleat]);
-                }
-                trSopaLletres.addView(btn);
+                trSopaLletres.addView(btnr);
+
+                Button finalBtnr = btnr;
+                btnr.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        System.out.println(click);
+                        click++;
+                        finalBtnr.setBackgroundColor(Color.RED);
+                        if (click > 4){
+                            //finalBtnr.setBackground();
+                        }
+                    }
+                });
             }
+            //btnc.setText(String.valueOf(Lletres[columna].charAt(1)));
+            //trSopaLletres.addView(btnc);
+
+            //trSopaLletres.addView(btnr);
         }
     }
 
-    private void GenerarParaulesATrobar(){
+    private void GenerarParaulesATrobar(String[] Lletres){
         TableLayout tlParaulesATrobar = findViewById(R.id.tlParaulesATrobar);
         TableRow trParaulesATrobar = null;
 
@@ -68,13 +85,10 @@ public class SopaLletres extends AppCompatActivity {
             trParaulesATrobar = new TableRow(this);
             tlParaulesATrobar.addView(trParaulesATrobar);
 
-            for (int fila = 0; fila < Lletres[columna].length; fila++) {
+            TextView tv = new TextView(this);
+            tv.setText(String.valueOf(Lletres[columna]));
 
-                TextView tv = new TextView(this);
-                tv.setText(Lletres[columna][fila]);
-
-                trParaulesATrobar.addView(tv);
-            }
+            trParaulesATrobar.addView(tv);
         }
     }
 }
